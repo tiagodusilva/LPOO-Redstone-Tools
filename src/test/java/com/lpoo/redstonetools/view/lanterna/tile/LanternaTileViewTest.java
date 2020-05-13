1d735cc84a07372313e3e788d2840eeb233e8cf3
Telmooo <telmoabaptista@hotmail.com>
2020-05-14 00:05:54 +0100

    Begin of view testing

---
+++ src/test/java/com/lpoo/redstonetools/view/lanterna/tile/LanternaTileViewTest.java
@@ -0,0 +1,21 @@
+package com.lpoo.redstonetools.view.lanterna.tile;
+
+import com.lpoo.redstonetools.model.utils.Power;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class LanternaTileViewTest {
+
+    private LanternaTileView tileView;
+
+    @Before
+    public void setup() {
+        this.tileView = Mockito.mock(LanternaTileView.class, Mockito.withSettings().defaultAnswer(Mockito.CALLS_REAL_METHODS));
+    }
+
+    @Test
+    public void testGetPowerColor() {
+        String minPowerColor = tileView.getPowerColor(Power.getMin());
+    }
+}
